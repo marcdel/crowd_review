8e32f27b05cc032787eaa2d2313d8afe758ec168
Marc Delagrammatikas <delagrammatikas@gmail.com>
2019-04-23 19:07:57 -0700

    Landing page and app layout updates

--- test/crowd_review_web/controllers/page_controller_test.exs
+++ test/crowd_review_web/controllers/page_controller_test.exs
@@ -3,6 +3,9 @@ defmodule CrowdReviewWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
+
     assert html_response(conn, 200) =~ "Crowd Review"
+    assert html_response(conn, 200) =~ "Give a Review"
+    assert html_response(conn, 200) =~ "Get a Review"
   end
 end
